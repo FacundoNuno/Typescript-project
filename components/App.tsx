dc94e806c497b5ab31b674da764867a5d69918b0
Facundo nuno <103080547+FacundoNuno@users.noreply.github.com>
2023-12-11 18:49:01 -0300

    backend ok.

---
+++ components/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App: React.FC = () => {
+    return (
+        <div>
+            ¡Hola, mundo!
+        </div>
+    );
+};
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,3 @@
+import App from './components/App'; // Importante no poner la extensión .tsx sino arrojara un error
+
+ReactDOM.render(<App />, document.querySelector('#root'));
